946acaa76ea4ba994744798e78af007ceadf6dfc
Lokrip <slava.dorokhov.08@mail.ru>
2025-04-24 17:34:48 +0600

    Init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
+import NextFederationPlugin from "@module-federation/nextjs-mf";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+//   webpack: (config, options) => {
+//     const { isServer } = options;
+//     config.plugins.push(
+//       new NextFederationPlugin({
+//         name: 'host',
+//         remotes: {// this will contain remote route
+//           remoteApp: `remoteApp@http://localhost:3001/_next/static/${
+//             isServer ? 'ssr' : 'chunks'
+//           }/remoteEntry.js`,
+//         },
+//         filename: 'static/chunks/remoteEntry.js',
+//         exposes: {
+//         },
+//         shared: {
+//           react: {
+//             singleton: true,
+//             eager: true,
+//             requiredVersion: false,
+//           },
+//           'react-dom': {
+//             singleton: true,
+//             eager: true,
+//             requiredVersion: false,
+//           },
+//         },
+//         extraOptions: {}
+//       }),
+//     );
+
+//     return config
+//   }
 };
 
 export default nextConfig;
