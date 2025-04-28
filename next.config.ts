f6ce96142dbb6b607b9302bb9769ab93594d6edd
Lokrip <slava.dorokhov.08@mail.ru>
2025-04-28 14:11:05 +0600

    chore: add rewrites auth service

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,15 @@
-import NextFederationPlugin from "@module-federation/nextjs-mf";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-//   webpack: (config, options) => {
-//     const { isServer } = options;
-//     config.plugins.push(
-//       new NextFederationPlugin({
-//         name: 'host',
-//         remotes: {// this will contain remote route
-//           remoteApp: `remoteApp@http://localhost:3001/_next/static/${
-//             isServer ? 'ssr' : 'chunks'
-//           }/remoteEntry.js`,
-//         },
-//         filename: 'static/chunks/remoteEntry.js',
-//         exposes: {
-//         },
-//         shared: {
-//           react: {
-//             singleton: true,
-//             eager: true,
-//             requiredVersion: false,
-//           },
-//           'react-dom': {
-//             singleton: true,
-//             eager: true,
-//             requiredVersion: false,
-//           },
-//         },
-//         extraOptions: {}
-//       }),
-//     );
-
-//     return config
-//   }
+    reactStrictMode: true,
+    async rewrites () {
+      return [
+        {
+            source: "/auth/:path*",
+            destination: "http://localhost:3002/auth/:path*"
+        }
+      ]
+    }
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
 "use client"
 
-import { sum_number } from "@packages/shared";
-import { useState } from "react";
-
+import Link from "next/link";
 
 export default function Home() {
-    const [number, setNumber] = useState<number>(0);
-
-    const onSumNumber = () => {
-        setNumber(sum_number(number, 20))
-    }
-
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <h1>{number}</h1>
-        <button onClick={() => setNumber(prev => prev + 1)}>Increment</button>
-        <button onClick={onSumNumber}>Sum</button>
+      <div className="flex flex-col items-start">
+        <nav>
+            <Link href={"/auth/sign-in"}>Login</Link>
+        </nav>
       </div>
     );
 }
