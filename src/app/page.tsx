1cf471d76aefb199c5eaed2580b813a2fbb068b3
Lokrip <slava.dorokhov.08@mail.ru>
2025-05-01 15:22:12 +0600

    feat: add shared package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-"use client"
-
-import Link from "next/link";
-
 export default function Home() {
     return (
       <div className="flex flex-col items-start">
-        <nav>
-            <Link href={"/auth/sign-in"}>Login</Link>
-        </nav>
+        Home
       </div>
     );
 }
